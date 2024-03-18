7a42e414dc96b4facb11f5d608be638959bab437
AJ Sorbello <ajsorbello@gmail.com>
2024-03-18 14:58:03 -0700

    fix app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { extractLocations, getEvents } from './api';
 
 import './App.css';
@@ -14,18 +14,18 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
 
- const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
-      allEvents :
-      allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  }
-
-   useEffect(() => {
-  fetchData();
+const fetchData = useCallback(async () => {
+  const allEvents = await getEvents();
+  const filteredEvents = currentCity === "See all cities" ?
+    allEvents :
+    allEvents.filter(event => event.location === currentCity)
+  setEvents(filteredEvents.slice(0, currentNOE));
+  setAllLocations(extractLocations(allEvents));
 }, [currentCity, currentNOE]);
+
+useEffect(() => {
+  fetchData();
+}, [fetchData]);
 
    return (
     <div className="App">
