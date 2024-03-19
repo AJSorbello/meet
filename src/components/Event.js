afbd72b82ef1eceae721275de6a8bf8079d24e11
AJ Sorbello <ajsorbello@gmail.com>
2024-03-19 11:23:40 -0700

    updated event date

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,13 +3,17 @@ import React, { useState } from 'react';
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   const start = new Date(event.start.dateTime).toLocaleTimeString();
-  const end = new Date(event.end.dateTime).toLocaleTimeString(); 
+  const end = new Date(event.end.dateTime).toLocaleTimeString();
+  const startDate = new Date(event.start.dateTime).toLocaleDateString();
+  const endDate = new Date(event.end.dateTime).toLocaleDateString();
 
   return (
     <li className="event">
       <h2>{event.summary}</h2>
       <p>{event.location}</p>
+      <p>Start Date: {startDate}</p>
       <p>Start Time: {start}</p>
+      <p>End Date: {endDate}</p>
       <p>End Time: {end}</p> 
       {showDetails && <p className="details">{event?.description}</p>}
       <button className="details-btn" onClick={() => setShowDetails(!showDetails)}>
