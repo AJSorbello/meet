1d5e3d4eaed936a648ec590e9612cc4f8c5b2042
AJ Sorbello <ajsorbello@gmail.com>
2024-04-01 12:18:59 -0700

    added if navigator.online

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-
+import NProgress from "nprogress";
 import mockData from "./mock-data";
 
 export const extractLocations = (events) => {
@@ -64,22 +64,24 @@ export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   }
-  // if (!navigator.onLine) {
-  //   const events = localStorage.getItem("lastEvents");
-  //   NProgress.done();
-  //   return events?JSON.parse(events):[];
-  // }
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
 
   const token = await getAccessToken();
 
   if (token) {
     removeQuery();
     const url =  "https://fhaj104cs8.execute-api.us-east-1.amazonaws.com/dev/api/get-events/" + token;
-    const response = await fetch(url);
+   const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
-    } else return null; 
+    } else return null;
   }
 };
 const removeQuery = () => {
