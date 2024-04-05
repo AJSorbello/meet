94e6711585d6788bb31fb8c0848c8b51685c7618
AJ Sorbello <ajsorbello@gmail.com>
2024-04-05 09:32:02 -0700

    fixed bug at citysearch onclick

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,16 +1,23 @@
-
 import { useState, useEffect } from "react";
+import useOnclickOutside from "react-cool-onclickoutside"
 
 const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [expanded, setExpanded] = useState(false);
 
-useEffect(() => {
-  setSuggestions(allLocations);
-}, [allLocations, setSuggestions]);
+  const ref = useOnclickOutside((e) => {
+  if (expanded && !e.target.classList.contains('suggestions')) {
+    setExpanded(false);
+  }
+});
 
-const handleInputChanged = (event) => {
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [allLocations, setSuggestions]);
+
+  const handleInputChanged = (event) => {
     const value = event.target.value;
     const filteredLocations = allLocations ? allLocations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
@@ -28,36 +35,38 @@ const handleInputChanged = (event) => {
     setInfoAlert(infoText);
   };
   
-const handleItemClicked = (event) => {
+  const handleItemClicked = (event) => {
     const value = event.target.textContent;
     setQuery(value);
     setShowSuggestions(false); // to hide the list
     setCurrentCity(value);
     setInfoAlert("")
   };
+
   return (
-    <div id="city-search">
-     <input
+    <div id="city-search" ref={ref}>
+ <input
   type="text"
   className="city"
-  id="citySearch" // added id attribute
+  id="citySearch"
   placeholder="Search for a city"
   value={query}
-  onFocus={() => setShowSuggestions(true)}
+  onFocus={() => { setShowSuggestions(true); setExpanded(true);}}
+  onBlur={() => { setTimeout(() => setShowSuggestions(false), 100); }}
   onChange={handleInputChanged}
 />
-         {showSuggestions ?
-        <ul className="suggestions">
-            {suggestions.map((suggestion) => {
-            return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
-          })}
-         <li key='See all cities' onClick={handleItemClicked}>
-            <b>See all cities</b>
-          </li>
-        </ul>
-        : null
-      }
+  {showSuggestions && expanded && (
+    <ul className="suggestions">
+      {suggestions.map((suggestion) => {
+        return <li onClick={handleItemClicked} key={suggestion}>{suggestion}</li>
+      })}
+      <li key='See all cities' onClick={handleItemClicked}>
+        <b>See all cities</b>
+      </li>
+    </ul>
+  )}
+</div>
+  );
+};
 
-    </div>
-  )};
 export default CitySearch;
